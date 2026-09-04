6e2c4688823c47e7323cdbcc6d3f8cba2cf2a20a
Kurnikova Milana <wirlzakz@gmail.com>
2026-09-04 09:54:06 +0300

    Разделить проект на три слоя

--- ServiceJournal/ServiceJournal/Program.cs
+++ ServiceJournal/ServiceJournal/Program.cs
@@ -1,22 +1,2 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using System.Windows.Forms;
-
-namespace ServiceJournal
-{
-    internal static class Program
-    {
-        /// <summary>
-        /// Главная точка входа для приложения.
-        /// </summary>
-        [STAThread]
-        static void Main()
-        {
-            Application.EnableVisualStyles();
-            Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
-        }
-    }
-}
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
