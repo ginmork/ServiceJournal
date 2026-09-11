113bf3e2912edd63ca6db1f3ed0a676bcf0985d2
Kurnikova Milana <wirlzakz@gmail.com>
2026-09-11 11:35:59 +0300

    Добавить выбор хранилища параметром запуска

--- ServiceJournal/ServiceJournal/Program.cs
+++ ServiceJournal/ServiceJournal/Program.cs
@@ -1,12 +1,32 @@
 ﻿using ServiceJournal.Data;
 using ServiceJournal.Logic;
 
-IRepairRepository repository = new RepairRepository();
+string jsonPath = Path.Combine(AppContext.BaseDirectory, "repairs.json");
+string xmlPath  = Path.Combine(AppContext.BaseDirectory, "repairs.xml");
+
+string kind = args.Length > 0 ? args[0] : "json";
+
+IRepairRepository repository = kind switch
+{
+    "xml"    => new XmlRepairRepository(xmlPath),
+    "memory" => new RepairRepository(),
+    "demo"   => new DemoRepairRepository(),
+    _        => new JsonRepairRepository(jsonPath)
+};
+
+Console.WriteLine($"Хранилище: {kind}");
 
 var service = new RepairService(repository);
 
-Console.WriteLine("Незавершённые ремонты:");
+Console.Write("Название оборудования: ");
+string equipment = Console.ReadLine() ?? "";
+
+service.AddRepair(equipment, "Нет");
+
+Console.WriteLine("Отобранные записи:");
 foreach (var item in service.GetImportant())
 {
     Console.WriteLine($"{item.Id}: {item.Equipment} - {item.IsDone}");
 }
+
+Console.WriteLine($"Незавершённых ремонтов: {service.GetCount()}");
